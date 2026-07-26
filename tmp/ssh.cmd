e031f65902dff4dc10dc0e53e9efc7f9136aabb6
adriano56789 <adrianomdk23@gmail.com>
2026-07-26 05:34:58 -0300

    chore: remove deployment scripts, update .gitignore

--- tmp/ssh.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-echo MshrUfZrh09hWr# | ssh -o StrictHostKeyChecking=no root@2.25.192.154 "echo CONNECTED && ls -la /root/srs/ && echo ---NGINX--- && ls -la /app/nginx/ 2>/dev/null && echo ---BACKEND--- && ls -la /app/backend/ 2>/dev/null"
